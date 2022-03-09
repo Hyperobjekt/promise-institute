65f071e750ea6b387ee8a1236c9998638a1f47ef
Corey Tegeler <coreytegeler@gmail.com>
2022-03-09 17:56:55 -0500

    add console test

--- packages/promise-theme/src/utils/getAttrs.js
+++ packages/promise-theme/src/utils/getAttrs.js
@@ -2,15 +2,15 @@ import React from "react";
 
 const getAttrs = (props) => {
 	const attrs = {};
+	console.log(props);
 	if(!props || !props.children) return attrs;
-
 	(Array.isArray(props.children) ? props.children : [props.children]).forEach(c => {
-		if(!c.props.hasOwnProperty("data-prop")) return;
 		const key = c.props["data-prop"];
+		if(!key) return;
 		let val;
-		if(c.props.hasOwnProperty("data-json")) {
+		if(c.props["data-json"]) {
 			val = JSON.parse(c.props["data-json"]);
-		} else if(c.props.hasOwnProperty("data-value")) {
+		} else if(c.props["data-value"]) {
 			val = c.props["data-value"]
 		} else {
 			val = c.props.children;
