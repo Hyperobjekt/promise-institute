d81a8db63b8eac696ae94efd3b94062844275d0f
Corey Tegeler <coreytegeler@gmail.com>
2022-03-09 10:45:50 -0500

    temp fix frontity index

--- packages/promise-theme/src/components/index.js
+++ packages/promise-theme/src/components/index.js
@@ -1,18 +1,15 @@
 import { connect, css, Global, Head, styled } from "frontity";
 import tw from "twin.macro";
-
 import globalStyles from "./../styles/style.css";
 import coreBlocksStyles from "./../styles/blocks/core.css";
 import themeBlockStyles from "./../styles/blocks/theme.css";
-
 import SiteHeader from "./site-header";
 import Content from "./content";
 import SiteFooter from "./site-footer";
-
 const Layout = ({ state, actions, libraries }) => {
 	const site = state ? state.frontity : null;
 	const data = state && site ? state.source.get(state.router.link) : null;
-	const post = state && data ? state.source[data.type][data.id] : null;
+	const post = state && data && data.type && data.id ? state.source[data.type][data.id] : null;
 	return (
 		<>
 			<Head>
